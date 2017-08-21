71284b89fe22b59c4f896610c44497068dc485f7
HomerHulk <GitHub-HomerHulk@users.noreply.github.com>
2017-08-21 23:00:15 +0200

    Test 01
    test ajout c'est pas bien

---
+++ js/devinette-final.js
@@ -0,0 +1,14 @@
+var nbUser = Number(prompt());
+
+    if((nbUser <= 0) && (nbUser >= 101)){
+         console.log("Votre nombre doit être compris entre 0 et 100");
+            }else if(nbUser > 18){
+                console.log (nbUSer + "est trop grand");
+            }else if(nbUser < 18){
+                console.log(nbUser + "est trop petit");
+            }else if{
+            console.log(nbUser + "est la bonne réponse");
+            }else{
+            	console.log("C' est pas bien de tricher");
+            
+            }
